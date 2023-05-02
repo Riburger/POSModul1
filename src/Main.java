ba162414fb322680cfbaa78190b8385fca3c9094
Nokran <117357352+Nokran@users.noreply.github.com>
2023-05-02 14:50:24 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hallo Julian");
         System.out.println("Hallo Riccardo");
+        System.out.println("Test");
     }
 }
