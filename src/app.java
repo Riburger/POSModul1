746489aac8be4eee21bee24d0f660bc54ec455d9
Nokran <117357352+Nokran@users.noreply.github.com>
2023-05-02 16:52:45 +0200

    Eine weitere Klasse hinzugefügt
    Ich habe für Testzwecke eine weitere Klasse Buch hinzugefügt.

--- src/app.java
+++ src/app.java
@@ -6,9 +6,12 @@ public class app{
 
         VHS heman = new VHS("He-Man and the Masters of Universe",120.99,"Büro UG", 120, "Ausgezeichnet" );
         Cartridge zelda = new Cartridge(" Zelda - Ocarina of Time", 999, "Tresor", true, "Nintendo", Konsolentyp.N64);
+        Buch herr = new Buch("Herr der Ringe", 49.99, "Wohnzimmer OG", "1312");
+
 
         bib.mediumHinzufügen(heman);
         bib.mediumHinzufügen(zelda);
+        bib.mediumHinzufügen(herr);
 
         bib.alleMedienAusgeben();
 
